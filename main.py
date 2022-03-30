f686341e738970ba45e241fb36389cb1d8ed029d
JoshDaFishy <computergeek328@gmail.com>
2022-03-30 19:55:59 +0100

    added grid system, trying to create scale now

--- main.py
+++ main.py
@@ -4,24 +4,48 @@ from pygame.locals import *
 with open("data.json") as f:
     data = json.load(f)
 
-coords = []
-
-for i in range(0,len(data["x"])):
-    pointx = data["x"][i]
-    pointy = data["y"][i]
-    coords.append((pointx,pointy))
-
-
+screenx = 800
+screeny = 800
 running = True
 pygame.init()
 clock = pygame.time.Clock()
 angle = 0
-screen = pygame.display.set_mode(size=(800,600))
-original_surf = pygame.Surface((50,50), pygame.SRCALPHA)
+screen = pygame.display.set_mode(size=(screenx,screeny))
+original_surf = pygame.Surface((10,10), pygame.SRCALPHA)
 original_surf.fill((255,0,0))
+coords = []
+
+
+def drawGrid():
+    squarecount = 0
+    blockSize = 20 #Set the size of the grid block
+    for x in range(0, screenx, blockSize):
+        for y in range(0, screeny, blockSize):
+            if not (x < blockSize*4 or screeny - y < blockSize*5):
+                rect = pygame.Rect(x, y, blockSize, blockSize)
+                pygame.draw.rect(screen, ((10,10,10)), rect, 1)
+                squarecount += 1
+    pygame.draw.lines(screen, (0,0,0), False, [(blockSize*4,0),(blockSize*4,(screeny-blockSize*4)),(blockSize*4,screeny-blockSize*4),(screenx,(screeny-blockSize*4))], 1)
+    return squarecount,blockSize*4,blockSize*5
+    # ((x/blockSize)-3)
+
+outputdata = drawGrid()
+squarecounter = outputdata[0]
+marginx = outputdata[1]
+marginy = outputdata[2]
+
+for i in range(0,len(data["x"])):
+    pointx = (((data["x"][i]/squarecounter) * 1000) + marginx)
+    pointy = (((data["y"][i]/squarecounter) * 1000) + marginy)
+    coords.append((pointx,pointy))
+
 
 while running:
-    screen.fill((0,0,0))
+    screen.fill((255,255,255))
+    outputdata = drawGrid()
+    squarecounter = outputdata[0]
+    marginx = outputdata[1]
+    marginy = outputdata[2]
     clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -31,16 +55,16 @@ while running:
                 running = False
 
     for i in range(0,len(data["x"])):
-        surf = pygame.transform.rotate(original_surf, angle)
+        surf = pygame.transform.rotate(original_surf, angle)   
         surfrect = surf.get_rect()
-        surfrect.centerx = (data["x"][i])
-        surfrect.centery = (data["y"][i])
+        surfrect.centerx = (((data["x"][i]/squarecounter) * 1000) + marginx)
+        surfrect.centery = (((data["y"][i]/squarecounter) * 1000) + marginy)
         screen.blit(surf,(surfrect))
     pygame.draw.lines(screen,((255,0,0)),False,coords, 1)
     pygame.display.flip()
-    angle += 1
-    print(angle)
-    if angle == 90:
-        angle = 0 
+    # angle += 1
+    # print(angle)
+    # if angle == 90:
+    #     angle = 0 
 
 pygame.quit()
--- test.py
+++ test.py
@@ -1,13 +1,25 @@
 import pygame
 from pygame.locals import *
 
+screenx = 800
+screeny = 600
 running = True
 pygame.init()
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode(size=(800,600))
+screen = pygame.display.set_mode(size=(screenx,screeny))
+
+
+def drawGrid():
+    blockSize = 20 #Set the size of the grid block
+    for x in range(0, screenx, blockSize):
+        for y in range(0, screeny, blockSize):
+            if not (x < blockSize*4 or screeny - y < blockSize*4):
+                rect = pygame.Rect(x, y, blockSize, blockSize)
+                pygame.draw.rect(screen, ((10,10,10)), rect, 1)
+    pygame.draw.lines(screen, (0,0,0), False, [(blockSize*4,0),(blockSize*4,(screeny-blockSize*4)),(blockSize*4,screeny-blockSize*4),(screenx,(screeny-blockSize*4))], 1)
 
 while running:
-    screen.fill((0,0,0))
+    screen.fill((255,255,255))
     clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -15,8 +27,9 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-    
-    
+    drawGrid()
+
+
     pygame.display.flip()
 
 
